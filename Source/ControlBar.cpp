20f25dd6ba9bca42cb57f34d5baf6725a49a752a
CS400V8-PC\Audio <fleming.paxton@yahoo.com>
2024-04-15 14:17:56 -0700

    rearrange control bar

--- Source/ControlBar.cpp
+++ Source/ControlBar.cpp
@@ -24,9 +24,13 @@ void ControlBar::paint(juce::Graphics& g)
 {
     g.setColour(juce::Colours::white);
     g.setFont(Chomp::Overhead.withHeight(38.f));
-    g.drawText("Ya Boy Pax    CHOMP  Multiband Clipper", 50, 2, 500, 18, juce::Justification::centred, false);
+    g.drawText("Ya Boy Pax", 10, 2, getWidth(), 18, juce::Justification::left, false);
 
-    juce::Rectangle<float> iconBounds = { 183.f, 2.f, 100.f, 27.5f };
+    g.drawText("CHOMP", 10, 2, getWidth(), 18, juce::Justification::centred, false);
+
+    g.drawText("Multiband Clipper", 10, 2, getWidth() - 120, 18, juce::Justification::centredRight, false);
+
+    juce::Rectangle<float> iconBounds = { static_cast<float>(getWidth()) / 2.f - 100.f, 2.f, 100.f, 27.5f };
     icon.drawWithin(g, iconBounds, juce::Justification::centred, 1.f);
 }
 
@@ -35,5 +39,5 @@ void ControlBar::resized()
     auto bounds = getLocalBounds();
 
     globalBypassButton.setBounds(getWidth() - 54, getY() + 4, 50, getHeight() - 8);
-    settingsButton.setBounds(globalBypassButton.getX() - 55, getY() + 4, 50, getHeight() - 8);
+    settingsButton.setBounds(globalBypassButton.getX() - 44, getY() + 4, 50, getHeight() - 8);
 }
