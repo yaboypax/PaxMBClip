9924880e90ba7459ef6e4132e1d0c5693e41fd56
CS400V8-PC\Audio <fleming.paxton@yahoo.com>
2024-05-16 00:53:47 -0700

    replace FIR with Convolution

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -32,6 +32,7 @@ enum class PhaseResponse
     minimum
 };
 
+#include <JuceHeader.h>
 class PaxMBClipAudioProcessor : public juce::AudioProcessor, public juce::ChangeBroadcaster
 #if JucePlugin_Enable_ARA
     , public juce::AudioProcessorARAExtension
@@ -135,8 +136,8 @@ private:
                         HP1, LP2,
                         HP2;
     //Linear Phase Filters
-    using FIRFilter = juce::dsp::ProcessorDuplicator<juce::dsp::FIR::Filter<float>, juce::dsp::FIR::Coefficients<float>>;
-    juce::dsp::ProcessorChain<FIRFilter, FIRFilter> FIR1, FIR2, FIR3, FIR4, FIR5;
+    juce::dsp::Convolution FIR1, FIR2, FIR3, FIR4, FIR5;
+    juce::AudioBuffer<float> impulseResponse1, impulseResponse2, impulseResponse3, impulseResponse4, impulseResponse5;
 
     // OversamplingiHP4; filters (butterworth)
     using OversamplingFilter = Dsp::SimpleFilter <Dsp::Butterworth::LowPass <kFOrder>, 2>;
@@ -174,40 +175,30 @@ private:
 
     void createFIRFilters()
     {
-        std::vector<float> impulseResponse1L, impulseResponse1R;
-        std::vector<float> impulseResponse2L, impulseResponse2R;
-        std::vector<float> impulseResponse3L, impulseResponse3R;
-        std::vector<float> impulseResponse4L, impulseResponse4R;
-        std::vector<float> impulseResponse5L, impulseResponse5R;
+        auto sampleRate = getSampleRate();
+        auto stereo = juce::dsp::Convolution::Stereo::yes;
+        auto trim = juce::dsp::Convolution::Trim::no;
+        auto normalise = juce::dsp::Convolution::Normalise::no;
+
 
         // Capture the impulse responses for each filter and channel
-        captureImpulseResponse(LP1, impulseResponse1L, impulseResponse1R);
-        captureImpulseResponse(AP2, impulseResponse2L, impulseResponse2R);
-        captureImpulseResponse(HP1, impulseResponse3L, impulseResponse3R);
-        captureImpulseResponse(LP2, impulseResponse4L, impulseResponse4R);
-        captureImpulseResponse(HP2, impulseResponse5L, impulseResponse5R);
+        captureImpulseResponse(LP1, impulseResponse1);
+        captureImpulseResponse(AP2, impulseResponse2);
+        captureImpulseResponse(HP1, impulseResponse3);
+        captureImpulseResponse(LP2, impulseResponse4);
+        captureImpulseResponse(HP2, impulseResponse5);
 
         // Initialize FIR filters using the coefficients
-        *FIR1.get<0>().state = juce::dsp::FIR::Coefficients<float>(impulseResponse1L.data(), impulseResponse1L.size());
-        *FIR1.get<1>().state = juce::dsp::FIR::Coefficients<float>(impulseResponse1R.data(), impulseResponse1R.size());
-
-        *FIR2.get<0>().state = juce::dsp::FIR::Coefficients<float>(impulseResponse2L.data(), impulseResponse2L.size());
-        *FIR2.get<1>().state = juce::dsp::FIR::Coefficients<float>(impulseResponse2R.data(), impulseResponse2R.size());
-
-        *FIR3.get<0>().state = juce::dsp::FIR::Coefficients<float>(impulseResponse3L.data(), impulseResponse3L.size());
-        *FIR3.get<1>().state = juce::dsp::FIR::Coefficients<float>(impulseResponse3R.data(), impulseResponse3R.size());
-
-        *FIR4.get<0>().state = juce::dsp::FIR::Coefficients<float>(impulseResponse4L.data(), impulseResponse4L.size());
-        *FIR4.get<1>().state = juce::dsp::FIR::Coefficients<float>(impulseResponse4R.data(), impulseResponse4R.size());
-
-        *FIR5.get<0>().state = juce::dsp::FIR::Coefficients<float>(impulseResponse5L.data(), impulseResponse5L.size());
-        *FIR5.get<1>().state = juce::dsp::FIR::Coefficients<float>(impulseResponse5R.data(), impulseResponse5R.size());
+        FIR1.loadImpulseResponse(std::move(impulseResponse1), sampleRate, stereo, trim, normalise);
+        FIR2.loadImpulseResponse(std::move(impulseResponse2), sampleRate, stereo, trim, normalise);
+        FIR3.loadImpulseResponse(std::move(impulseResponse3), sampleRate, stereo, trim, normalise);
+        FIR4.loadImpulseResponse(std::move(impulseResponse4), sampleRate, stereo, trim, normalise);
+        FIR5.loadImpulseResponse(std::move(impulseResponse5), sampleRate, stereo, trim, normalise);
     }
-
-    void captureImpulseResponse(juce::dsp::LinkwitzRileyFilter<float>& filter, std::vector<float>& responseLeft, std::vector<float>& responseRight)
+    void captureImpulseResponse(juce::dsp::LinkwitzRileyFilter<float>& filter, juce::AudioBuffer<float>&outputBuffer)
     {
-        responseLeft.resize(kImpulseSize, 0.0f);
-        responseRight.resize(kImpulseSize, 0.0f);
+        outputBuffer.clear();
+        outputBuffer.setSize(2, kImpulseSize);
 
         // Create an impulse signal for both channels
         juce::AudioBuffer<float> impulseBuffer(2, kImpulseSize);
@@ -219,24 +210,22 @@ private:
         juce::dsp::ProcessContextReplacing<float> context(block);
         filter.process(context);
 
-        // Copy the response for left and right channels separately
-        for (int i = 0; i < kImpulseSize; ++i)
+        for (int channel = 0; channel < impulseBuffer.getNumChannels(); ++channel)
         {
-            responseLeft[i] = impulseBuffer.getSample(0, i);
-            responseRight[i] = impulseBuffer.getSample(1, i);
+            outputBuffer.copyFrom(channel, 0, impulseBuffer, channel, 0, kImpulseSize);
         }
     }
 
     void forwardBackwardProcess(juce::AudioBuffer<float>& buffer,
-        juce::dsp::ProcessorChain<FIRFilter, FIRFilter>& filter)
+        juce::dsp::Convolution& filter)
     {
         juce::ScopedNoDenormals noDenormals;
-        const int groupDelay = kImpulseSize / 2;
+        const int groupDelay = (kImpulseSize - 1)/2;
 
         const int numChannels = buffer.getNumChannels();
         const int numSamples = buffer.getNumSamples();
 
-        juce::AudioBuffer<float> processingBuffer(numChannels, numSamples + 2 * groupDelay);
+        juce::AudioBuffer<float> processingBuffer(numChannels, numSamples);
         processingBuffer.clear();
 
         // Initial group delay offset
